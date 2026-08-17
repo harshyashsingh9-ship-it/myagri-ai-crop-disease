ebc2409ea2c5baf015689790005b6b48ef8a5827
Yashasvi Singh <harshyashsingh9@gmail.com>
2026-08-17 22:29:06 +0530

    augmentation.py

--- src/data/src/data/augmentation.py
+++ src/data/src/data/augmentation.py
@@ -1,1 +1,26 @@
+"""
+Image augmentation pipeline.
+"""
 
+from torchvision import transforms
+
+
+def get_train_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(
+            brightness=0.2,
+            contrast=0.2,
+            saturation=0.2
+        ),
+        transforms.ToTensor(),
+    ])
+
+
+def get_validation_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
